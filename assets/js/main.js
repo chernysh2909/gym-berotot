9f226169a25837eb551632a356bc023943a3198f
alex <chernysh2909@gmail.com>
2022-08-10 10:55:05 +0300

    header, nav

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// show menu
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if(navToggle) {
+  navToggle.addEventListener("click", ()=>{
+    navMenu.classList.add("show-menu")
+  })
+}
+if(navClose){
+  navClose.addEventListener('click', ()=>{
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+//hide mobi menu  when click link
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = ()=>{
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n=> n.addEventListener('click', linkAction))
