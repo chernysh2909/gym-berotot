1f223916de90e61a705bca33bc6e4123aab7beda
alex <chernysh2909@gmail.com>
2022-08-10 14:42:28 +0300

    program

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,11 @@ const linkAction = ()=>{
   navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n=> n.addEventListener('click', linkAction))
+
+//change bg header
+
+const scrollHeader = ()=>{
+  const header = document.getElementById('header')
+  this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
