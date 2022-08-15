81014855a171ec5465ead5beb686858ff4326c5b
alex <chernysh2909@gmail.com>
2022-08-15 15:15:55 +0300

    scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,3 +96,11 @@ const scrollActive = () =>{
 }
 
 window.addEventListener('scroll', scrollActive)
+
+//scroll up
+const scrollUp = ()=>{
+  const scrollUp  = document.getElementById('scroll-up')
+
+  this.scrollY >=350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
